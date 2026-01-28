7b4b9355a76fc4cc412f7d6e225cc29b9d70a9be
Marc-Antoine Berger <mab.dev69@gmail.com>
2026-01-28 23:53:32 +0100

    finished the configuration of frontend. Tailwind and Shadcn installation and creation of the two layout for watchers and admin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/admin/Dashboard'
+
+const PublicLayout = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <Outlet />
+    </div>
+  )
+}
+
+const AdminLayout = () => {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <nav className="p-4 bg-slate-900 text-white">NavBar Admin (Temporaire)</nav>
+      <main className="p-4">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route path="/dashboard" element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/admin/Dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Dashboard Admin</h1>
+      <p>Gestion des examens et des surveillants.</p>
+    </div>
+  )
+}
