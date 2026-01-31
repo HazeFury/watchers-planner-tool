6cd8d0e074aa897ca6ee8a16fb791a18c7f31c5f
HazeFury <mab.dev69@gmail.com>
2026-01-31 18:19:55 +0100

    added a CRUD for exams + protection of routes by role

---
+++ server/src/exams/exams.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateExamDto } from './dto/create-exam.dto';
+import { UpdateExamDto } from './dto/update-exam.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class ExamsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createExamDto: CreateExamDto) {
+    return this.prisma.exam.create({
+      data: createExamDto,
+    });
+  }
+
+  findAll() {
+    return this.prisma.exam.findMany({
+      orderBy: {
+        startTime: 'asc',
+      },
+    });
+  }
+
+  async findOne(id: number) {
+    const exam = await this.prisma.exam.findUnique({
+      where: { id },
+    });
+    if (!exam) {
+      throw new NotFoundException(`Examen #${id} introuvable`);
+    }
+    return exam;
+  }
+
+  async update(id: number, updateExamDto: UpdateExamDto) {
+    await this.findOne(id); 
+
+    return this.prisma.exam.update({
+      where: { id },
+      data: updateExamDto,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+    return this.prisma.exam.delete({
+      where: { id },
+    });
+  }
+}
