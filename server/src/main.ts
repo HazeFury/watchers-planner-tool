fd14e95ee16e3611e9101474aef3e70bad591701
HazeFury <mab.dev69@gmail.com>
2026-02-25 23:55:19 +0100

    added a checkAuth function in backend to check if user's cookie is stil valid

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  });
+
   app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe({
